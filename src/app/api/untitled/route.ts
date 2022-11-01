5c25c07de99dfb97c13a5ec247cabe9175948289
safdar-azeem <safdarazeem00@gmail.com>
2022-11-01 18:10:00 +0000

    Add route.ts

---
+++ src/app/api/untitled/route.ts
@@ -0,0 +1,26 @@
+import { createUntitled } from '@/actions/createUntitled'
+import { auth } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+
+export async function POST() {
+	try {
+		const { userId } = auth()
+
+		if (!userId) {
+			return new Response('Unauthorized', { status: 401 })
+		}
+
+		const { id } = await createUntitled(userId)
+
+		return NextResponse.json(id, { status: 201 })
+	} catch (error: any) {
+		return NextResponse.json(
+			{
+				message:
+					error.message ||
+					'Something went wrong when create untiled document',
+			},
+			{ status: 500 }
+		)
+	}
+}
