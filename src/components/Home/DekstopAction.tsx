54c1c9f7150d634d341b755513e221858fc5d415
safdar-azeem <safdarazeem00@gmail.com>
2022-11-24 00:00:00 +0000

    Add DekstopAction.tsx

---
+++ src/components/Home/DekstopAction.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import { buttonVariants } from '../ui/Button'
+import NavMenu from './NavMenu'
+import { cn } from '@/lib/utils'
+
+const DekstopAction: React.FC = () => {
+	const pathName = usePathname()
+
+	return (
+		<nav
+			aria-label='Primary Navigation'
+			className='flex-1 hidden lg:flex justify-between items-center h-[30px]'>
+			{pathName !== '/mobile-nav' && (
+				<>
+					{/* Left Menu */}
+					<NavMenu />
+
+					{/* Right Menu */}
+					<div className='flex gap-2'>
+						<Link
+							className={cn(
+								buttonVariants({
+									variant: 'ghost',
+									size: 'default',
+								}),
+								'text-base'
+							)}
+							href='/sign-in'>
+							Log in
+						</Link>
+						<Link
+							className={cn(
+								buttonVariants({
+									variant: 'default',
+									size: 'default',
+								}),
+								'text-base'
+							)}
+							href='/sign-up'>
+							Get Notion free
+						</Link>
+					</div>
+				</>
+			)}
+		</nav>
+	)
+}
+
+export default DekstopAction
