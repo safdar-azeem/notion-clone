09dd7c65d813d6983c86fc923d2d96e117cfc671
safdar-azeem <safdarazeem00@gmail.com>
2022-12-20 13:45:00 +0000

    Add SignIn.tsx

---
+++ src/components/auth/SignIn.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { SignIn as SignInClerk, ClerkLoaded } from '@clerk/nextjs'
+import Link from 'next/link'
+import Loader from './Loader'
+import CloseModal from '../CloseModal'
+import { useShowMobileNav } from '@/hooks/use-show-mobile-nav'
+
+interface SignInProps {
+	isModal?: boolean
+	isPage?: boolean
+}
+
+const SignIn: React.FC<SignInProps> = ({ isModal = false, isPage }) => {
+	const { showMobileNav } = useShowMobileNav()
+
+	return (
+		<div className='relative min-h-[260.25px]'>
+			<Loader />
+			<ClerkLoaded>
+				{isModal && (
+					<div className='closeModal'>
+						<CloseModal />
+					</div>
+				)}
+				<SignInClerk signUpUrl='/sign-up' />
+				<div className='hideAfterClerkLoaded absolute text-sm bottom-[48px] left-[61px]'>
+					<span className='text-primary/60'>No account? </span>
+					{isPage ? (
+						<a
+							href='/sign-up'
+							className='focus-visible:ring-2 ring-gray-500'>
+							Sign up
+						</a>
+					) : (
+						<Link
+							href={`/sign-up${
+								showMobileNav ? '?showMobileNav=1' : ''
+							}`}
+							replace={isModal}
+							className='focus-visible:ring-2 ring-gray-500'>
+							Sign up
+						</Link>
+					)}
+				</div>
+			</ClerkLoaded>
+		</div>
+	)
+}
+
+export default SignIn
