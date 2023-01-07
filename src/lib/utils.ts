9ac8ee708debdcb0ebf482b9a238f0486b0541f6
safdar-azeem <safdarazeem00@gmail.com>
2023-01-07 14:00:00 +0000

    Add utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,80 @@
+import { type ClassValue, clsx } from "clsx";
+import { MouseEvent } from "react";
+import { twMerge } from "tailwind-merge";
+import { formatDistanceToNowStrict } from "date-fns";
+import locale from "date-fns/locale/en-US";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+type Options = {
+  // eslint-disable-next-line no-unused-vars
+  callback?: ((...args: never[]) => unknown) | null;
+  noPreventDefault?: boolean;
+};
+
+export const preventBubbling = (
+  event: MouseEvent<HTMLElement>,
+  options?: Options
+) => {
+  if (options?.noPreventDefault !== true) event.preventDefault();
+
+  event.stopPropagation();
+
+  // Perform any additional logic based on the options/callback if needed.
+  if (options?.callback) options.callback();
+};
+
+const formatDistanceLocale = {
+  lessThanXSeconds: "just now",
+  xSeconds: "just now",
+  halfAMinute: "just now",
+  lessThanXMinutes: "{{count}}m",
+  xMinutes: "{{count}}m",
+  aboutXHours: "{{count}}h",
+  xHours: "{{count}}h",
+  xDays: "{{count}}d",
+  aboutXWeeks: "{{count}}w",
+  xWeeks: "{{count}}w",
+  aboutXMonths: "{{count}}m",
+  xMonths: "{{count}}m",
+  aboutXYears: "{{count}}y",
+  xYears: "{{count}}y",
+  overXYears: "{{count}}y",
+  almostXYears: "{{count}}y",
+};
+
+function formatDistance(token: string, count: number, options?: any): string {
+  options = options || {};
+
+  const result = formatDistanceLocale[
+    token as keyof typeof formatDistanceLocale
+  ].replace("{{count}}", count.toString());
+
+  if (options.addSuffix) {
+    if (options.comparison > 0) {
+      return "in " + result;
+    } else {
+      if (result === "just now") return result;
+      return result + " ago";
+    }
+  }
+
+  return result;
+}
+
+export function formatTimeToNow(date: Date): string {
+  return formatDistanceToNowStrict(date, {
+    addSuffix: true,
+    locale: {
+      ...locale,
+      formatDistance,
+    },
+  });
+}
+
+export function isValidObjectID(str: string) {
+  const hexRegex = /^[0-9a-fA-F]{24}$/;
+  return hexRegex.test(str);
+}
